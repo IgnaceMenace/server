d2762bd56b00f9a2db0760286e73cbcfa87e09ed
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-12-05 22:11:19 +0100

    Create run.sh

---
+++ nextcloud/run.sh
@@ -0,0 +1,11 @@
+# For x64 CPUs:
+sudo docker run \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 8080:8080 \
+-e APACHE_PORT=11000 \
+-e SKIP_DOMAIN_VALIDATION=true \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest
