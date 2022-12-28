6d9e42488623580b466473ed589685f4891f8c78
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-12-28 23:23:09 +0100

    Delete nginxProxyManager.sh

--- nginxProxyManager.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p docker/nginxProxyManager
-cd docker/nginxProxyManager
-wget https://raw.githubusercontent.com/IgnaceMenace/server/main/nginxProxyManager/docker-compose.yml
-sudo docker compose up -d
