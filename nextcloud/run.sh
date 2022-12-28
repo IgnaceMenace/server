1c1de09ddb9aafedeca561312097f6c3d51e33ea
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2022-12-28 19:24:01 +0100

    Update run.sh

--- nextcloud/run.sh
+++ nextcloud/run.sh
@@ -10,6 +10,6 @@ sudo docker run \
 -e APACHE_PORT=11000 \
 # if you can't validate your domain uncomment this (but i think you still need to write a domain but it s going to by pass 
 #-e SKIP_DOMAIN_VALIDATION=true \
---volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
---volume /var/run/docker.sock:/var/run/docker.sock:ro \
+-v nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+-v /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest
