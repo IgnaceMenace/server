730a3af0744f7fbcc2ae6e7fbc7d787a5d141c40
IgnaceMenace <74070019+IgnaceMenace@users.noreply.github.com>
2024-12-24 01:11:53 +0100

    Update setupFedS.sh

--- setupFedS.sh
+++ setupFedS.sh
@@ -19,9 +19,8 @@ sudo fwupdmgr update
 sudo dnf install \
 https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 -y
-sudo dnf config-manager \
---add-repo \
-https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y install dnf-plugins-core
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
 # Remove some software
 sudo dnf remove docker \
